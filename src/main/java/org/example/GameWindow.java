6c7c051d9cf5e007c910fa4bfe0d184026d28738
Patryk Ruczynski <ptrkk17@gmail.com>
2023-08-25 15:39:49 +0200

    Adding Game class and GamePanel into JFrame.

--- src/main/java/org/example/GameWindow.java
+++ src/main/java/org/example/GameWindow.java
@@ -4,12 +4,13 @@ import javax.swing.*;
 
 public class GameWindow extends JFrame {
     private JFrame jFrame;
-    public GameWindow() {
+    public GameWindow(GamePanel gamePanel) {
         jFrame = new JFrame();
 
         jFrame.setSize(400,400);
         // EXIT when we close the program
         jFrame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        jFrame.add(gamePanel);
         jFrame.setVisible(true);
     }
 }
